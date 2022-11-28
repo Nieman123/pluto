c3a1ce2dcc5eded9b853ac935e4d16730796037e
Nieman <nieman123@gmail.com>
2022-11-28 09:56:11 -0500

    artist descriptions

--- lib/tabs/artists.dart
+++ lib/tabs/artists.dart
@@ -146,89 +146,155 @@ class _ArtistState extends State<Artist> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Column(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 15.0, vertical: 15.0),
-                                      child: Container(
-                                        width: 350.0,
-                                        height: 350.0,
-                                        decoration: const BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            image: DecorationImage(
-                                                fit: BoxFit.fill,
-                                                image: NetworkImage(
-                                                    'https://i.imgur.com/2NAb0MC.png'))),
+                                SizedBox(
+                                  width: (width * .8) / 2,
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 15.0, vertical: 15.0),
+                                        child: Container(
+                                          width: 350.0,
+                                          height: 350.0,
+                                          decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              image: DecorationImage(
+                                                  fit: BoxFit.fill,
+                                                  image: NetworkImage(
+                                                      'https://i.imgur.com/2NAb0MC.png'))),
+                                        ),
                                       ),
-                                    ),
-                                    CustomText(
-                                        text: 'RHiNO',
-                                        fontSize: 18,
-                                        color: Theme.of(context)
-                                            .primaryColorLight),
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 15.0, vertical: 15.0),
-                                      child: Container(
-                                        width: 350.0,
-                                        height: 350.0,
-                                        decoration: const BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            image: DecorationImage(
-                                                fit: BoxFit.fill,
-                                                image: NetworkImage(
-                                                    'https://i.imgur.com/ph79TGT.png'))),
+                                      Column(
+                                        children: [
+                                          CustomText(
+                                              text: 'RHiNO',
+                                              fontSize: 18,
+                                              color: Theme.of(context)
+                                                  .primaryColorLight),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: RichText(
+                                              text: const TextSpan(
+                                                text:
+                                                    'RHiNO is a DJ, producer and visual artist from Asheville, NC.',
+                                                style: TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                            ),
+                                          )
+                                        ],
                                       ),
-                                    ),
-                                    CustomText(
-                                        text: 'DIVINE THUD',
-                                        fontSize: 18,
-                                        color: Theme.of(context)
-                                            .primaryColorLight),
-                                  ],
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 15.0, vertical: 15.0),
+                                        child: Container(
+                                          width: 350.0,
+                                          height: 350.0,
+                                          decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              image: DecorationImage(
+                                                  fit: BoxFit.fill,
+                                                  image: NetworkImage(
+                                                      'https://i.imgur.com/ph79TGT.png'))),
+                                        ),
+                                      ),
+                                      Column(
+                                        children: [
+                                          CustomText(
+                                              text: 'DIVINE THUD',
+                                              fontSize: 18,
+                                              color: Theme.of(context)
+                                                  .primaryColorLight),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: RichText(
+                                              text: const TextSpan(
+                                                text:
+                                                    'Divine Thud is a DJ, producer and visual artist from Asheville, NC.',
+                                                style: TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                                Column(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 15.0, vertical: 15.0),
-                                      child: Container(
-                                        width: 350.0,
-                                        height: 350.0,
-                                        decoration: const BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            image: DecorationImage(
-                                                fit: BoxFit.fill,
-                                                image: NetworkImage(
-                                                    'https://i.imgur.com/5I4TqyV.jpg'))),
+                                SizedBox(
+                                  width: (width * .8) / 2,
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 15.0, vertical: 15.0),
+                                        child: Container(
+                                          width: 350.0,
+                                          height: 350.0,
+                                          decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              image: DecorationImage(
+                                                  fit: BoxFit.fill,
+                                                  image: NetworkImage(
+                                                      'https://i.imgur.com/5I4TqyV.jpg'))),
+                                        ),
                                       ),
-                                    ),
-                                    CustomText(
-                                        text: 'NIEMAN',
-                                        fontSize: 18,
-                                        color: Theme.of(context)
-                                            .primaryColorLight),
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 15.0, vertical: 15.0),
-                                      child: Container(
-                                        width: 350.0,
-                                        height: 350.0,
-                                        decoration: const BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            image: DecorationImage(
-                                                fit: BoxFit.fill,
-                                                image: NetworkImage(
-                                                    'https://i.imgur.com/6Drkikb.jpg'))),
+                                      Column(
+                                        children: [
+                                          CustomText(
+                                              text: 'NIEMAN',
+                                              fontSize: 18,
+                                              color: Theme.of(context)
+                                                  .primaryColorLight),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: RichText(
+                                              text: const TextSpan(
+                                                text:
+                                                    'Inspired by Dirty Bird and Off the Grid records, Nieman is a DJ and producer from Asheville, NC.',
+                                                style: TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                            ),
+                                          )
+                                        ],
                                       ),
-                                    ),
-                                    CustomText(
-                                        text: 'DJ DAGGETT',
-                                        fontSize: 18,
-                                        color: Theme.of(context)
-                                            .primaryColorLight),
-                                  ],
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 15.0, vertical: 15.0),
+                                        child: Container(
+                                          width: 350.0,
+                                          height: 350.0,
+                                          decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              image: DecorationImage(
+                                                  fit: BoxFit.fill,
+                                                  image: NetworkImage(
+                                                      'https://i.imgur.com/6Drkikb.jpg'))),
+                                        ),
+                                      ),
+                                      Column(
+                                        children: [
+                                          CustomText(
+                                              text: 'DJ DAGGETT',
+                                              fontSize: 18,
+                                              color: Theme.of(context)
+                                                  .primaryColorLight),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: RichText(
+                                              text: const TextSpan(
+                                                text:
+                                                    'Originally from Orlando, FL, DJ Dagget is spinning every thing from throwbacks, to the latest tech house tunes.',
+                                                style: TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ],
                             )
